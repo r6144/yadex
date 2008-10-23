9b5cd944f944ed0c34e95fe12422d37069898265
r6144 <rainy6144@gmail.com>
2008-10-23 23:43:11 +0800

    More small changes.

--- src/zinfo.cc
+++ src/zinfo.cc
@@ -6,12 +6,14 @@ bool LineTypeHasTag(wad_ldtype_t type)
 {
   if (yg_level_format != YGLF_HEXEN) return (type != 0);
   else return (type > 0 && ! (type == 181 /* slope */
-			      || (type >= 80 && type <= 84) /* script */));
+			      || (type >= 80 && type <= 84) /* script */
+			      || type == 121 /* lineid */));
 }
 
 bool LineTypeHasNonvisualAction(wad_ldtype_t type)
 {
   if (yg_level_format != YGLF_HEXEN) return (type != 0);
   else return (type > 0 && ! (type == 181 /* slope */
-			      || (type >= 100 && type <= 103) /* scroll */));
+			      || (type >= 100 && type <= 103) /* scroll */
+			      || type == 121 /* lineid */));
 }
